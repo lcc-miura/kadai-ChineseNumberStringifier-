9467d64bf631c3199872e056bf018be3dd78fdaa
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-01-24 20:47:25 +0900

    修正

--- src/main/java/org/example/BiIntFunction.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface BiIntFunction<T, R> {
-    public R apply(T t, int n);
-}
--- src/main/java/org/example/ChineseNumberStringifier.java
+++ src/main/java/org/example/ChineseNumberStringifier.java
@@ -1,49 +1,53 @@
 package org.example;
 
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ChineseNumberStringifier {
-    private final Map<String, String> intChineseMap = new HashMap<>() {
+    private final Map<Integer, String> chineseNumMap = new HashMap<>() {
         {
-            put("1", "壱");
-            put("2", "弐");
-            put("3", "参");
-            put("4", "肆");
-            put("5", "伍");
-            put("6", "陸");
-            put("7", "漆");
-            put("8", "捌");
-            put("9", "玖");
+            put(1, "壱");
+            put(2, "弐");
+            put(3, "参");
+            put(4, "肆");
+            put(5, "伍");
+            put(6, "陸");
+            put(7, "漆");
+            put(8, "捌");
+            put(9, "玖");
+        }
+    };
+    private final Map<Integer, String> chineseRankMap = new HashMap<>() {
+        {
+            put(1, "拾");
+            put(2, "佰");
+            put(3, "阡");
+            put(4, "萬");
         }
     };
 
     String stringify(int n) {
         String str = Integer.valueOf(n).toString();
-        List<String> strArr = Arrays.asList(str.split(""));
-        Collections.reverse(strArr);
+        List<String> strList = reversal(Arrays.asList(str.split("")));
+        List<String> chineseStrList = IntStream
+                .range(0, strList.size())
+                .mapToObj(i -> {
+                    String s = strList.get(i);
+                    if(s.equals("0")) return "";
+                    String chineseNum = chineseNumMap.get(Integer.parseInt(s));
+                    if(i == 0) return chineseNum;
+                    return chineseNum + chineseRankMap.get(i);
+                })
+                .toList();
+        List<String> fixedStrList = reversal(chineseStrList);
+        return String.join("", fixedStrList);
+    };
 
-        List<String> chineseStrArr = strArr.stream().map(WithIndex.apply(0, (s, i) -> {
-            if(s.equals("0")) return "";
-            String chineseStr = intChineseMap.get(s);
-            switch(i){
-                case 1:
-                    chineseStr += "拾";
-                    break;
-                case 2:
-                    chineseStr += "佰";
-                    break;
-                case 3:
-                    chineseStr += "阡";
-                    break;
-                case 4:
-                    chineseStr += "萬";
-                    break;
-            }
-            return chineseStr;
-        })).collect(Collectors.toList());
-
-        Collections.reverse(chineseStrArr);
-        return String.join("", chineseStrArr);
+    private List<String> reversal(List<String> list) {
+        List<String> newList = new ArrayList<>();
+        for(int i = 0; i < list.size(); i++) {
+            newList.add(0, list.get(i));
+        }
+        return newList;
     };
 }
