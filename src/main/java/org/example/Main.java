0e49df5d5c316c37204851f5ecde4ed97b915180
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-01-26 13:28:07 +0900

    単体テストを実装

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         AbstractStringifier cns = new ChineseNumberStringifier();
-        cns.stringify(45013);
+        String result1 = cns.stringify(45013);
+        System.out.println(result1);
 
         AbstractStringifier rns = new RomanNumberStringifier();
-        cns.stringify(1024);
+        String result2 = rns.stringify(1024);
+        System.out.println(result2);
     }
 }
